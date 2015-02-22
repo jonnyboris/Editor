ace7dae7e38e84bcd361baed7aa8ba91c1ea15ec
Jonny Fairfull <jonny@bbrdigital.com>
2015-02-22 21:53:41 +0000

    Added focus and old bookmarklet

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,8 @@
 var editor = document.getElementById("editor"),
-    throttled = _.throttle(save, 2000);
+    throttled = _.debounce(save, 1000);
 
 function save() {
     var content =  editor.innerHTML;
-    console.log(content);
     localStorage.setItem("content", content);
 }
 
@@ -16,4 +15,5 @@ window.onload = function() {
     } else {
         editor.innerHTML = "This is a simple text editor, it automatically saves your text, locally to your computer. So if you close the tab it should still be here when you get back. ";
     }
+    editor.focus();
 }
