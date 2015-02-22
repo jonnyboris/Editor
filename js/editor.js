c55aab5947ca773d5793c1525073e91b28e4dba1
Jonny Fairfull <jonny@bbrdigital.com>
2015-02-22 01:54:09 +0000

    Updated default text

--- js/editor.js
+++ js/editor.js
@@ -14,6 +14,6 @@ window.onload = function() {
     if(content) {
         editor.innerHTML = content;
     } else {
-        editor.innerHTML = "This is a simple text editor, it should automatically save your text. So if you close the tab it should still be here when you get back.";
+        editor.innerHTML = "This is a simple text editor, it automatically saves your text, locally to your computer. So if you close the tab it should still be here when you get back. ";
     }
 }
